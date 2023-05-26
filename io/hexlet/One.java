d0ec0de0d887b25593b2ef7bcbcd2dd683f29c54
Yan Maloedov <yanmaloedov@gmail.com>
2023-05-26 23:15:40 +0300

    io

---
+++ io/hexlet/One.java
@@ -0,0 +1,6 @@
+package io.hexlet;
+public class One {
+    public static void main(String[] args) {
+        Two.greeting();
+    }
+}
---
+++ io/hexlet/Two.java
@@ -0,0 +1,6 @@
+package io.hexlet;
+public class Two {
+    public static void greeting() {
+        System.out.println("Java for Brave");
+    }
+}
